26d8de123f972ffa8195e3aace16df926ccbc00a
Neo2003 <Neo.2003@Hotmail.fr>
2016-11-28 20:26:06 +0100

    Make compilation select proper PI to optimize Use 'build.sh' to build instead of 'make'
    Signed-off-by: Neo2003 <Neo.2003@Hotmail.fr>

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+HARDWARE=`uname -m`
+if [ "$HARDWARE" = "armv6l" ] ; then
+    echo Building for Pi 1
+    rm Makefile
+    cp Makefile_Pi1 Makefile
+    make
+elif [ "$HARDWARE" = "armv7l" ] ; then
+    REVISION=`cat /proc/cpuinfo | grep Revision |  cut -d : -f2 | tr -d '[:space:]'`
+    if [ "$REVISION" = "a01041" ] || [ "$REVISION" = "a21041" ] ; then
+        echo Building for Pi 2
+        rm Makefile
+        cp Makefile_Pi2 Makefile
+        make -j4
+    else
+        echo Building for Pi 3
+        rm Makefile
+        cp Makefile_Pi3 Makefile
+        make -j4
+    fi
+fi
